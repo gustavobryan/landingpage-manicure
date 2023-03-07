64daecd3804c0501da690ee599993236d93d1a09
Gustavo Bryan <devgustavob@gmail.com>
2023-03-07 20:07:11 -0300

    :bricks: ci adição do js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav__menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+})
