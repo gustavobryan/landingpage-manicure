0a50372738b6b5d84c0dd62213ed7cb4e0ff569b
Gustavo Bryan <devgustavob@gmail.com>
2023-03-09 13:12:14 -0300

    :dizzy: adição de um carrossel de imagens

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
 const menu = document.querySelector('.menu');
 const navMenu = document.querySelector('.nav__menu');
+const imgs = document.getElementById('img');
+const img = document.querySelectorAll('#img img');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     navMenu.classList.toggle('ativo');
-})
+});
+
+let idx = 0;
+
+function carrossel () {
+    idx++;
+
+    if(idx > img.length -2) {
+        idx = 0;
+    }
+
+      imgs.style.transform = `translateX(${-idx * 350}px)`;
+}
+
+setInterval(carrossel, 1800);
